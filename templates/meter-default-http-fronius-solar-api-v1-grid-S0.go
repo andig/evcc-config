1214ccc9ee92c818dd4b2614e1fa2c036e41dcfd
BuildTools <unconfigured@null.spigotmc.org>
2021-02-02 12:16:39 +0100

    Update Fronius Solar API V1 templates

--- templates/meter-default-http-fronius-solar-api-v1-grid-S0.go
+++ templates/meter-default-http-fronius-solar-api-v1-grid-S0.go
@@ -9,10 +9,10 @@ func init() {
 		Class:  "meter",
 		Type:   "default",
 		Name:   "Fronius Solar API V1 (grid S0 meter/ HTTP)",
-		Sample: `power: # pv power reading Fronius Solar API V1 GetPowerFlowRealtimeData.P_Grid
-        type: http # use http plugin for grid power (P_Grid)
-        uri: http://192.0.2.2/solar_api/v1/GetPowerFlowRealtimeData.fcgi
-        jq: if .Body.Data.Site.P_Grid == null then 0 else .Body.Data.Site.P_Grid end # parse GetPowerFlowRealtimeData P_Grid response`,
+		Sample: `power: # Grid power reading Fronius Solar API V1 GetPowerFlowRealtimeData.P_Grid
+  type: http # use http plugin for grid power (P_Grid)
+  uri: http://192.0.2.2/solar_api/v1/GetPowerFlowRealtimeData.fcgi
+  jq: if .Body.Data.Site.P_Grid == null then 0 else .Body.Data.Site.P_Grid end # parse GetPowerFlowRealtimeData P_Grid response`,
 	}
 
 	registry.Add(template)

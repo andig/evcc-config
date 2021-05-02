ead8066ec63206b2a9d895057a70abee9dcaf95a
Alex Stumpf <github@AlexStumpf.de>
2021-05-02 22:08:16 +0200

    ''Zweck''

--- templates/meter-default-http-shelly3em.go
+++ templates/meter-default-http-shelly3em.go
@@ -8,7 +8,7 @@ func init() {
 	template := registry.Template{
 		Class:  "meter",
 		Type:   "default",
-		Name:   "Shelly 3EM (HTTP)",
+		Name:   "Shelly 3EM (HTTP) / General Purpose Meter",
 		Sample: `power: # power reading
 - type: http # use http
   uri: http://192.0.2.2/status
